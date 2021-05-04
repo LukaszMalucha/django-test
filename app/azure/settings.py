3686da0886933a3c2030c5833e798876885aee41
Lukasz Malucha <lucasmalucha@gmail.com>
2021-05-04 12:47:21 +0100

    data

--- app/azure/settings.py
+++ app/azure/settings.py
@@ -205,10 +205,10 @@ AUTH_ADFS = {
     'CLIENT_SECRET': os.environ.get("AZURE_SECRET"),
     'CLAIM_MAPPING': {'first_name': 'given_name',
                       'last_name': 'family_name',
-                      'email': 'email'},
+                      'email': 'given_name'},
     'GROUPS_CLAIM': 'roles',
     'MIRROR_GROUPS': True,
-    'USERNAME_CLAIM': 'email',
+    'USERNAME_CLAIM': 'given_name',
     'TENANT_ID': os.environ.get("AZURE_TENANT_ID"),
     'RELYING_PARTY_ID': os.environ.get("AZURE_CLIENT_ID"),
 }
