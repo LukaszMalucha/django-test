4e54829bb2cd2aecd80fadd602b5fecf83d03727
Lukasz Malucha <lucasmalucha@gmail.com>
2022-01-31 14:33:17 +0000

    data

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import home
+from core.views import home, failed_response
 
 app_name = "core"
 
 urlpatterns = [
     path("", home, name="home"),
+    path("failed_response", failed_response, name="failed_response")
 
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -7,3 +7,11 @@ def home(request):
     messages = MessageModel.objects.all()
 
     return render(request, "home.html", {"messages": messages})
+
+
+
+def failed_response(request, error_message, status):
+    # Return an error message
+    return render(request, 'myapp/login_failed.html', {
+        'error_message': error_message,
+    }, status=status)
